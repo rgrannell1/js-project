067473260ea67e3f99cd509e30f9906df3fee09c
John Strong <strngj411@gmail.com>
2013-07-10 21:49:15 +0100

    init implementation to the user api

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,42 @@
+(function() {
+	"use strict"
 
-alert(
-"just a standin for actual js files, presumably we will have many js files"
-)
+	var size = {},
+	canvas = "",
+	theme = "default";
+
+	// main Object
+	window.Col = {};
+
+	var setCanvas = function(ele, obj) {
+		if(document.getElementById(ele) !== undefined) {
+			canvas = document.getElementById(ele);
+			size.width = obj.width;
+			size.height = obj.height;
+		} else {
+			throw new Error();
+		}
+	};
+
+	Col.canvas = function(canvasId, dimensions) {
+		setCanvas(canvasId, dimensions);
+	};
+
+	Col.data = function(json) {
+		// parse json
+	};
+
+	Col.theme = function(theme) {
+		// set theme on layout
+		this.theme = theme;
+	};
+
+	Col.start = function() {
+		// call algorithm
+		// render pretty
+	};
+	
+	console.log(Col);
+	return Col;
+})();
+
