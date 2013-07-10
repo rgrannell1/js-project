cbe66f501ebaacfc51082346a591ab1f82505be9
rgrannell1 <r.grannell1@nuigalway.ie>
2013-07-10 02:33:24 +0100

    added is.js to a module, and added a function to accurately determine the class of a function

--- scripts/helper/is.js
+++ scripts/helper/is.js
@@ -1,12 +1,27 @@
 
-// TODO: add to define function
+define(function () {
+	// this object is included because the builtin tests for 
+	// properties in javascript is awful, and these should be 
+	// standardised between branches
 
-var is = {
-	// tests for certain types of values (functions, objects)
-	function: function (val) {
-		return typeof val === "function";
-	},
-	array: function (obj) {
-		// suitable test for arrayness
-	}
-}
+	return {
+		// tests for certain types of values (functions, objects)
+		toType: function (val) {
+			// found online, better than typeof at determining
+			// the class of an object
+
+			return ({}).toString.call(val).
+				match(/\s([a-zA-Z]+)/)[1].toLowerCase()
+		},
+		function: function (val) {
+			return toType(val) === "function";
+		},
+		array: function (val) {
+			return toType(val) === 'array';
+		},
+		object: function (val) {
+			toType(val) === 'object'
+		}
+	};
+})
+
