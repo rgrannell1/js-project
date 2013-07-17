b557e6a87db653b8397b0a1b613e730ed1d71905
John Strong <strngj411@gmail.com>
2013-07-17 22:53:29 +0100

    fixes some browser compatibility issues, add basic unit tests to test html file, switch element type required from canvas to div, and more.... to tired

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,19 @@
-// TODO: find a better way to return the object
+if(!Array.prototype.indexOf) {
+	// implement indexOf for browser support
+	console.log('making indexOf');
+
+	Array.prototype.indexOf = function(val) {
+		for(var i = 0; i < this.length; i++) {
+			if(val === this[i]) {
+				return i;
+			} else {
+				continue;
+			}
+		}
+		return -1;
+	}
+}
+
 (function(window) {
 "use strict"
 
@@ -15,7 +30,27 @@ var canvas,
 
 	_CPjs = window.CPjs,
 
-	CPjs = function() {};
+	CPjs = function() {},
+
+	is = {
+		toType: function (val) {
+			return ({}).toString.call(val).
+			match(/\s([a-zA-Z]+)/)[1].toLowerCase()
+		},
+		function: function (val) {
+			return toType(val) === "function";
+		},
+		array: function (val) {
+			return toType(val) === 'array';
+		},
+		object: function (val) {
+			toType(val) === 'object'
+		},
+		numeric: function (val) {
+
+			return !isNaN(parseFloat(val)) && isFinite(val);
+		}
+	};
 
 	// create new Ajax Object (provide cross browser support)
 	function _getXmlHttp() {
@@ -30,7 +65,7 @@ var canvas,
 				try {
 					ajaxObject = new ActiveXObject("Msxml2.XMLHTTP");
 				} catch(ex) {
-					throw "browser does not support XMLHttpRequest";
+					throw new Error("browser does not support XMLHttpRequest");
 				}
 			}
 		}
@@ -43,17 +78,32 @@ var canvas,
 		if(document.getElementById(ele) !== null) {
 			var element = document.getElementById(ele);
 			
-			if(element.tagName !== 'CANVAS') {
-				console.error("element is not a canvas");
+			if(element.tagName !== 'DIV') {
+				throw new Error("element " + ele + " is not a div");
 			}
+		}
+	}
+
+	// set animation for collage
+	function animationSupported(prop) {
+		var animations = ["swift", "top", "bottom", "left", "right"];
+
+		if(animations.indexOf(prop) !== -1) {
+			return true;
+
+		} else {
+			return false;
 		}
 	}
 
 	// sets style options supplied by the user
 	function setCanvasOptions(additionalProps) {
+
 		options.greyscale = additionalProps.greyscale || false;
-		options.crop = additionalProps.crop || false;
-		options.background = additionalProps.canvasTransparent || false;
+		options.background = additionalProps.background || 'transparent';
+
+		animationSupported(additionalProps.animation) ? options.animateType = additionalProps.animation : options.animateType = null;
+		
 	}
 
 	// parse and store the paths to collage image files
@@ -65,16 +115,16 @@ var canvas,
 		}
 	}
 
-	// takes in a canvas id
-	CPjs.prototype.canvas =	function(canvasId) {
-		setCanvasPropery(canvasId);
+	// takes in a div id
+	CPjs.prototype.id = function(elementId) {
+		setCanvasPropery(elementId);
 		return this;
 	};
 
 	// parse json property with path
-	CPjs.prototype.data =	function(json) {
+	CPjs.prototype.images =	function(json) {
 		// create new XMLHttp object to handle request
-		// Note: must be relative to html filepath
+		// Note: must be relative to html filepath [currently breaks in ie10 and Chrome during local work.... a problem]
 		var request = _getXmlHttp();
 		request.open('GET', json.toString(), false);
 		request.send();
@@ -93,13 +143,12 @@ var canvas,
 	// take in canvas dimensions to be used by algorithm
 	CPjs.prototype.dimensions = function(w, h)	{
 		// check arguments supplied are Integers
-		if(typeof h === "number" && typeof w === "number" 
-			&&	h%1 == 0 && w%1 == 0) {
-			width = w;
-			height = h;
+		if(is.numeric(w) && is.numeric(h)) {
+			width = Math.floor(w);
+			height = Math.floor(h);
 
-		}else {
-			console.error("Integers must be supplied");
+		} else {
+			throw new Error("width and height must both be numbers");
 		}
 
 		return this;
@@ -110,12 +159,21 @@ var canvas,
 		return this;
 	};
 
-	// begin algorithm and render onto canvas
 	CPjs.prototype.start = function() {
-		console.log(this);
+		// build object to be used by algorithm
+		function buildObject() {
+			return {
+				width : width,
+				height : height,
+				options : options,
+				images : imagePaths
+			};
+		}
+
+		console.log(buildObject());
+		//call algorithm
 	}
 	
-	// return globally scoped CPjs object
 	window.CPjs = new CPjs();
 
 })(window);
