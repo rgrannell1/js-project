efb3c8b936d3600bf99070da3f113a26663c1e08
John Strong <strngj411@gmail.com>
2013-07-27 13:03:00 +0100

    center logic of the api for easier error handling

--- scripts/main.js
+++ scripts/main.js
@@ -16,21 +16,8 @@ if(!Array.prototype.indexOf) {
 (function(window) {
 "use strict"
 
-// private variables
-var imageCollage,
-
-	collageWidth,
-
-	collageHeight,
-
-	images = [],
-
-	_plaid = window.Plaid,
-
-	plaid = {},
-
 	// for better type checking
-	is = (function() {
+	var is = (function() {
 		return {
 				toType: function (val) {
 					return ({}).toString.call(val).
@@ -53,128 +40,121 @@ var imageCollage,
 
 	// theme objects that can be refrenced by the user
 	// the idea is that these themes are easily editable to users so simple themes can be easily created
-	themes = (function() {
-		return {
-				aqua : {
-					name : "aqua",
-					canvasStyle : {
-						padding : "padding : 4px;",
-						border : 'border : 2px solid rgba(150,150,240,1);',
-						boxShadow : 'box-shadow : 2px 2px 8px rgba(20, 80, 200, 0.5);'
-					},
-					imageStyle : {
-						padding : 'padding: 2px 2px;'
-					},
+	themes = {
+		aqua : {
+			name : "aqua",
+			canvasStyle : {
+				padding : "padding : 4px;",
+				border : 'border : 2px solid rgba(150,150,240,1);',
+				boxShadow : 'box-shadow : 2px 2px 8px rgba(20, 80, 200, 0.5);'
+			},
+			imageStyle : {
+				padding : 'padding: 2px 2px;'
+			},
 
-					imageEvents : {
-						onclick : function(evt) {
-							// open a dialog
-						},
-
-						onmouseover : function(evt) {
-
-							evt.currentTarget.style.transition = "yellow";
-						},
-
-						onmouseout : function(evt) {
-							evt.currentTarget.style.backgroundColor = "white";
-						}
-					}
+			imageEvents : {
+				onclick : function(evt) {
+					// open a dialog
 				},
 
-				ember : {
-					name : "ember",
-					canvasStyle : {
-						padding : "padding : 4px;",
-						border : 'border : 2px solid rgba(240,150,150, 1);',
-						boxShadow : 'box-shadow : 2px 2px 6px rgba(200, 20, 20, 0.5);'
-					},
-					imageStyle : {
-						padding: "padding: 2px 2px;"
-					},
-					
-					imageEvents : {
-						onclick : function(evt) {
-							// open a dialog
-							
-						},
+				onmouseover : function(evt) {
 
-						onmouseover : function(evt) {
-							//evt.currentTarget.style.backgroundColor = "yellow";
-						},
-
-						onmouseout : function(evt) {
-							//evt.currentTarget.style.backgroundColor = "white";
-						}
-					}
+					evt.currentTarget.style.transition = "yellow";
 				},
 
-				vanilla : {
-					name : "vanilla",
-					canvasStyle : {
-						padding : "padding : 4px;",
-						border : 'border : 1px solid rgba(180,180,180, 1);'
-					},
-
-					imageStyle : {
-						padding : 'padding: 2px 2px;'
-					},
-
-					imageEvents : {
-						onclick : function(evt) {
-							// open a dialog
-							
-						},
-
-						onmouseover : function(evt) {
-							evt.currentTarget.style.backgroundColor = "yellow";
-						},
-
-						onmouseout : function(evt) {
-							evt.currentTarget.style.backgroundColor = "white";
-						}
-					}
-				},
-
-				skylight : {
-					name : "skylight",
-					canvasStyle : {
-						padding : "padding : 4px;",
-						border : 'border : 3px solid rgba(80, 80, 95, 1);'
-					},
-
-					imageStyle : {
-						padding: 'padding: 2px 2px;'
-					},
-
-					imageEvents : {
-						onclick : function(evt) {
-							dialog.create();
-							dialog.render();
-						},
-
-						onmouseover : function(evt) {
-							evt.currentTarget.style.transition = "1s ease";
-							evt.currentTarget.style.webkitTransition = "1s ease";
-
-							evt.currentTarget.style.transform = "scale(1.02)";
-							evt.currentTarget.style.webkitTransform = "scale(1.02)";
-						},
-
-						onmouseout : function(evt) {
-							evt.currentTarget.style.transform = "scale(1)";
-							evt.currentTarget.style.webkitTransform = "scale(1)";
-						}
-					}
+				onmouseout : function(evt) {
+					evt.currentTarget.style.backgroundColor = "white";
 				}
-			};
-	})();
-	
-	// error handling 
-	function UsefulError(description) {
-		this.description = description;
-	}
+			}
+		},
 
+		ember : {
+			name : "ember",
+			canvasStyle : {
+				padding : "padding : 4px;",
+				border : 'border : 2px solid rgba(240,150,150, 1);',
+				boxShadow : 'box-shadow : 2px 2px 6px rgba(200, 20, 20, 0.5);'
+			},
+			imageStyle : {
+				padding: "padding: 2px 2px;"
+			},
+			
+			imageEvents : {
+				onclick : function(evt) {
+					// open a dialog
+					
+				},
+
+				onmouseover : function(evt) {
+					//evt.currentTarget.style.backgroundColor = "yellow";
+				},
+
+				onmouseout : function(evt) {
+					//evt.currentTarget.style.backgroundColor = "white";
+				}
+			}
+		},
+
+		vanilla : {
+			name : "vanilla",
+			canvasStyle : {
+				padding : "padding : 4px;",
+				border : 'border : 1px solid rgba(180,180,180, 1);'
+			},
+
+			imageStyle : {
+				padding : 'padding: 2px 2px;'
+			},
+
+			imageEvents : {
+				onclick : function(evt) {
+					// open a dialog
+					
+				},
+
+				onmouseover : function(evt) {
+					evt.currentTarget.style.backgroundColor = "yellow";
+				},
+
+				onmouseout : function(evt) {
+					evt.currentTarget.style.backgroundColor = "white";
+				}
+			}
+		},
+
+		skylight : {
+			name : "skylight",
+			canvasStyle : {
+				padding : "padding : 4px;",
+				border : 'border : 3px solid rgba(80, 80, 95, 1);'
+			},
+
+			imageStyle : {
+				padding: 'padding: 2px 2px;'
+			},
+
+			imageEvents : {
+				onclick : function(evt) {
+					dialog.create();
+					dialog.render();
+				},
+
+				onmouseover : function(evt) {
+					evt.currentTarget.style.transition = "1s ease";
+					evt.currentTarget.style.webkitTransition = "1s ease";
+
+					evt.currentTarget.style.transform = "scale(1.02)";
+					evt.currentTarget.style.webkitTransform = "scale(1.02)";
+				},
+
+				onmouseout : function(evt) {
+					evt.currentTarget.style.transform = "scale(1)";
+					evt.currentTarget.style.webkitTransform = "scale(1)";
+				}
+			}
+		}
+	};
+	
 	// for better storage of image properties
 	function Image(src, w, h) {
 		this.source = src;
@@ -184,19 +164,14 @@ var imageCollage,
 
 	// gets canvas element return and sets it to the canvas attribute
 	function _imageCollage(e) {
-		var element = document.getElementById(e);
-
-		if(element !== null && element.tagName === 'DIV') {
-				imageCollage = element;
-		} else {
-			throw new UsefulError("Collage container must be a DIV");
-		}
+		return document.getElementById(e);
 	}
 
 	// get images suppled by the user
-	function storeImages() {
+	function storeImages(id) {
 		// get elements within element supplied by the client
-		var imgs = imageCollage.getElementsByTagName('img');
+		var imgs = id.getElementsByTagName('img'),
+			images = [];
 
 		for(var i = 0; i< imgs.length; i++) {
 			var src = imgs[i].getAttribute('src'),
@@ -205,137 +180,134 @@ var imageCollage,
 
 			images.push(new Image(src, w, h));
 		}
+
+		return images;
 	};
 
+	function Render() {
 
-	var render = (function() {
-		// set background color of collage div to the same as its parent
-		// if CPjs-canvas-transparent is set to true
-		var collageHeight,
-			collageWidth;
+		this.theme = function() {
+			var id = this.id;
 
-		return {
-			
-				_dimensions : function(w, h) {
-					collageWidth = w;
-					collageHeight = h;
-				},
+			var theme,
+				t,
+				inheritId,
+				inheritedBgColor,
+				images;
 
-				_theme : function() {
-					var theme,
-						t,
-						inheritId,
-						inheritedBgColor,
-						images;
-
-					var setStyles = function(style) {
-						var output = "";
-						
-						if(style === 'canvasStyle' && inheritedBgColor !== null) {
-							output += "background-color : " + inheritedBgColor + ";";
-						}
-
-						for(var prop in theme[style]) {
-							if(theme[style].hasOwnProperty(prop)) {
-								output += theme[style][prop];
-							}
-						}
-
-						// add oveflow on images (probably not needed in the future)
-						output += "overflow: hidden;";
-
-						return output;
-					}
-
-					t = imageCollage.getAttribute("plaid-theme")
-					inheritId = imageCollage.getAttribute("plaid-inherit-backgroundColor"),
-					inheritedBgColor,
-					images = imageCollage.getElementsByTagName("img");
-
-					if(inheritId !== null) {
-						var inheritedBgColorElement = document.getElementById(inheritId);
-						inheritedBgColorElement !== null? inheritedBgColor = inheritedBgColorElement.style.backgroundColor : inheritedBgColor = null;
-					}
-
-					if(t !== null) {
-						
-						for(var data in themes) {
-							if(themes[data].name === t) {
-								theme = themes[data];
-								break;
-							}
-						}
-
-						if(theme !== undefined) {
-							// apply styles to the collage and images
-							imageCollage.setAttribute("style", setStyles('canvasStyle'));
-							
-							for(var i = 0; i < images.length ; i++) {
-								images[i].setAttribute("style", setStyles('imageStyle'));
-								
-								for(var evt in theme.imageEvents) {
-									images[i][evt.toString()] = theme.imageEvents[evt.toString()];
-								}
-								
-							}
-						}
-					}
-					
+			var setStyles = function(style) {
+				var output = "";
+				
+				if(style === 'canvasStyle' && inheritedBgColor !== null) {
+					output += "background-color : " + inheritedBgColor + ";";
 				}
-			};	
-	})();
 
-	function API(colId) {
-		this.id = colId;
-		this.width;
-		this.height;
+				for(var prop in theme[style]) {
+					if(theme[style].hasOwnProperty(prop)) {
+						output += theme[style][prop];
+					}
+				}
 
-		// tale in collage width and height
-		this.dimensions = function(w, h) {
-			if(is.numeric(w) && is.numeric(h)) {
-				var width = Math.floor(w);
-				var height = Math.floor(h);
+				// add oveflow on images (probably not needed in the future)
+				output += "overflow: hidden;";
 
-				render._dimensions(width, height)
-
-			} else {
-					throw new UsefulError("width and height must both be numbers");
+				return output;
 			}
 
-			return this;
-		};
+			t = id.getAttribute("plaid-theme")
+			inheritId = id.getAttribute("plaid-inherit-backgroundColor"),
+			inheritedBgColor,
+			images = id.getElementsByTagName("img");
 
-		// call algorithm and render
-		this.start = function() {
-			var theBackend = function(config, callback) {
+			if(inheritId !== null) {
+				var inheritedBgColorElement = document.getElementById(inheritId);
+				inheritedBgColorElement !== null? inheritedBgColor = inheritedBgColorElement.style.backgroundColor : inheritedBgColor = null;
+			}
+
+			if(t !== null) {
+				
+				for(var data in themes) {
+					if(themes[data].name === t) {
+						theme = themes[data];
+						break;
+					}
+				}
+
+				if(theme !== undefined) {
+					// apply styles to the collage and images
+					id.setAttribute("style", setStyles('canvasStyle'));
+					
+					for(var i = 0; i < images.length ; i++) {
+						images[i].setAttribute("style", setStyles('imageStyle'));
+						
+						for(var evt in theme.imageEvents) {
+							images[i][evt.toString()] = theme.imageEvents[evt.toString()];
+						}
+						
+					}
+				}
+			}
+		}
+	};
+
+	var theBackend = function(config, callback) {
 				console.log(config);
-				callback({});
-			};
+				callback([{},{},{}]);
+	};
 
-			try {
-				_imageCollage(this.id);
-				storeImages();
+		
+	function Plaid() {
+		this.id;
+		this.width;
+		this.height;
+		this.images;
+	}
+
+	Plaid.prototype.id = function(idEle) {
+		this.id = _imageCollage(idEle);
+
+		return this;
+	};
+
+	Plaid.prototype.dimensions = function(w, h) {
+		this.width = w;
+		this.height = h;
+
+		return this;
+	};
+
+	Plaid.prototype.start = function() {
+		var self = this;
+		var render = new Render();
+
+		try {
+			if(this.id !== null || this.id.tagName === 'DIV') {
+				this.images = storeImages(this.id);
 
 				var config = {
-					images : images
+					width: this.width,
+					height : this.height,
+					images : this.images
 				};
 
 				theBackend(config, function(val) {
-					render._theme(val);
+					//test
+					console.log(config);
+
+					render.theme.call(self);
 				});
 
-			} catch (e) {
-				console.error(e.description);
+			} else {
+				throw new TypeError("element used for collage must be a div");
 			}
-		};
-
-	}
-
-	// return object to window
-	var plaid = function(colId) {
-		return new API(colId);
+		} catch (e) {
+			console.error(e);
+		}
 	};
 
-	window.plaid = plaid;
+	var _plaid = window.plaid;
+
+	// return object to window
+	window.plaid =  new Plaid();
 
 })(window);
